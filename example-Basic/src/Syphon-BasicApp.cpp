0417b78753a06470a4d6ee6d339c4e7433b728a9
ragnaringi <ragnaringi@Ragnars-MacBook-Pro.local>
2017-10-17 18:22:03 +0100

    Updates basic example for Cinder 0.9.1

--- example-Basic/src/Syphon-BasicApp.cpp
+++ example-Basic/src/Syphon-BasicApp.cpp
@@ -31,12 +31,8 @@
 
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
-#include "cinder/ImageIo.h"
-#include "cinder/gl/Texture.h"
-#include "cinder/gl/GlslProg.h"
-#include "cinder/Rand.h"
-#include "cinder/Color.h"
 #include "cinder/gl/gl.h"
+
 #include "cinderSyphon.h"
 
 using namespace ci;
@@ -48,118 +44,92 @@ using namespace std;
 
 class SyphonBasicApp : public App {
 public:
-	static void prepareSettings( Settings *settings );
-	void keyDown( KeyEvent event );
-	void mouseDown( MouseEvent event );
-	void mouseUp( MouseEvent event );
-	void mouseMove( MouseEvent event );
-	void mouseDrag( MouseEvent event );
-	void setup();
-	void update();
-	void draw();
-    
-    gl::TextureRef	mLogo;
-	gl::GlslProgRef	mShader;
-	float			mAngle;
-	
-	syphonServer mScreenSyphon; //each item to publish requires a different server
-	syphonServer mTextureSyphon;
-	
-	syphonClient mClientSyphon; //our syphon client
+  static void prepareSettings( Settings *settings );
+  void setup() override;
+  void mouseDown( MouseEvent event ) override;
+  void update() override;
+  void draw() override;
+  
+  gl::TextureRef  mLogo;
+  gl::GlslProgRef  mShader;
+  float      mAngle;
+  
+  syphonServer mScreenSyphon; //each item to publish requires a different server
+  syphonServer mTextureSyphon;
+  
+  syphonClient mClientSyphon; //our syphon client
 };
 
 void SyphonBasicApp::prepareSettings( Settings *settings )
 {
-	settings->setWindowSize(WIDTH,HEIGHT);
-	settings->setFrameRate(60.f);
+  settings->setWindowSize(WIDTH,HEIGHT);
+  settings->setFrameRate(60.f);
 }
 
 void SyphonBasicApp::setup()
 {
-	try {
-		mLogo = gl::Texture::create( loadImage( loadAsset("cinder_logo_alpha.png") ) );
-	}
-	catch( ... ) {
-		std::cout << "unable to load the texture file!" << std::endl;
-	}
-	
-	try {
-		mShader = gl::GlslProg::create( loadAsset("passThru_vert.glsl"), loadAsset("gaussianBlur_frag.glsl") );
-	}
-	catch( gl::GlslProgCompileExc &exc ) {
-		std::cout << "Shader compile error: " << std::endl;
-		std::cout << exc.what();
-	}
-	catch( ... ) {
-		std::cout << "Unable to load shader" << std::endl;
-	}
-	
-	mAngle = 0.0f;
-	
-	mScreenSyphon.setName("Screen Output"); // set a name for each item to be published
-	mTextureSyphon.setName("Texture Output");
-	
-	mClientSyphon.setup();
-    
-	// in order for this to work, you must run simple server which is a syphon test application
-    // feel free to change the app and server name for your specific case
-    mClientSyphon.set("", "Simple Server");
-    
-    mClientSyphon.bind();
-}
-
-void SyphonBasicApp::update()
-{
-	mAngle += 0.05f;
-}
-
-void SyphonBasicApp::draw()
-{
-	gl::enableAlphaBlending();
-	gl::clear(Color::white());
-    gl::color(ColorA(1.f, 1.f, 1.f, 1.f));
-	
-	{
-		gl::ScopedGlslProg glsl( mShader );
-		gl::ScopedTextureBind tex( mLogo );
-		mShader->uniform( "tex0", 0 );
-		mShader->uniform( "sampleOffset", vec2( cos( mAngle ), sin( mAngle ) ) * ( 3.0f / getWindowWidth() ) );
-		gl::drawSolidRect( mLogo->getBounds() );
-	}
-    
-    mClientSyphon.draw(vec2(16.f, 64.f)); //draw our client image
-    
-	mScreenSyphon.publishScreen(); //publish the screen's output
-	mTextureSyphon.publishTexture(mLogo); //publish our texture without shader
-	
-	//anything that we draw after here will not be published
-    gl::drawStringCentered("This text will not be published to Syphon.", vec2(getWindowCenter().x, 20.f), ColorA::black());
-	
-}
-
-void SyphonBasicApp::keyDown( KeyEvent event )
-{
-	//
+  try {
+    mLogo = gl::Texture::create( loadImage( loadAsset("cinder_logo_alpha.png") ) );
+  }
+  catch( ... ) {
+    std::cout << "unable to load the texture file!" << std::endl;
+  }
+  
+  try {
+    mShader = gl::GlslProg::create( loadAsset("passThru_vert.glsl"), loadAsset("gaussianBlur_frag.glsl") );
+  }
+  catch( gl::GlslProgCompileExc &exc ) {
+    std::cout << "Shader compile error: " << std::endl;
+    std::cout << exc.what();
+  }
+  catch( ... ) {
+    std::cout << "Unable to load shader" << std::endl;
+  }
+  
+  mAngle = 0.0f;
+  
+  mScreenSyphon.setName("Screen Output"); // set a name for each item to be published
+  mTextureSyphon.setName("Texture Output");
+  
+  mClientSyphon.setup();
+  
+  // in order for this to work, you must run simple server which is a syphon test application
+  // feel free to change the app and server name for your specific case
+  mClientSyphon.set("", "Simple Server");
+  
+  mClientSyphon.bind();
 }
 
 void SyphonBasicApp::mouseDown( MouseEvent event )
 {
-	//
 }
 
-void SyphonBasicApp::mouseUp( MouseEvent event )
+void SyphonBasicApp::update()
 {
-	//
+  mAngle += 0.05f;
 }
 
-void SyphonBasicApp::mouseMove( MouseEvent event )
+void SyphonBasicApp::draw()
 {
-	//
-}
-
-void SyphonBasicApp::mouseDrag( MouseEvent event )
-{
-	//
+  gl::enableAlphaBlending();
+  gl::clear(Color::white());
+  gl::color(ColorA(1.f, 1.f, 1.f, 1.f));
+  
+  {
+    gl::ScopedGlslProg glsl( mShader );
+    gl::ScopedTextureBind tex( mLogo );
+    mShader->uniform( "tex0", 0 );
+    mShader->uniform( "sampleOffset", vec2( cos( mAngle ), sin( mAngle ) ) * ( 3.0f / getWindowWidth() ) );
+    gl::drawSolidRect( mLogo->getBounds() );
+  }
+  
+  mClientSyphon.draw(vec2(16.f, 64.f)); // draw our client image
+  
+  mScreenSyphon.publishScreen(); // publish the screen's output
+  mTextureSyphon.publishTexture(mLogo, false); // publish our texture without shader
+  
+  //anything that we draw after here will not be published
+  gl::drawStringCentered("This text will not be published to Syphon.", vec2(getWindowCenter().x, 20.f), ColorA::black());
 }
 
 CINDER_APP( SyphonBasicApp, RendererGl, SyphonBasicApp::prepareSettings )
