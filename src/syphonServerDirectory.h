eb7e2210e5cbbe6fc91a09372f6a1103bfd6d067
ragnaringi <ragnaringi@Ragnars-MacBook-Pro.local>
2017-10-19 17:52:59 +0100

    Updates SyphonServerDirectory example for Cinder 0.9.1

--- src/syphonServerDirectory.h
+++ src/syphonServerDirectory.h
@@ -17,19 +17,19 @@ class syphonServerDirectory;
 
 class syphonServerDescription {
 public:
-    syphonServerDescription(){
-        serverName = appName = "null";
-    }
-    
-    syphonServerDescription(std::string _serverName, std::string _appName) : serverName(_serverName), appName(_appName){
-        //
-    }
-    
-    friend bool operator== (const syphonServerDescription& lhs, const syphonServerDescription& rhs){
-        return (lhs.serverName == rhs.serverName) && (lhs.appName == rhs.appName);
-    }
-    
-    std::string serverName, appName;
+  syphonServerDescription(){
+    serverName = appName = "null";
+  }
+  
+  syphonServerDescription(std::string _serverName, std::string _appName) : serverName(_serverName), appName(_appName){
+    //
+  }
+  
+  friend bool operator== (const syphonServerDescription& lhs, const syphonServerDescription& rhs){
+    return (lhs.serverName == rhs.serverName) && (lhs.appName == rhs.appName);
+  }
+  
+  std::string serverName, appName;
 };
 
 typedef ci::signals::Signal<void( std::vector<syphonServerDescription> )> SyphonServerAnnouncedSignal;
@@ -38,43 +38,43 @@ typedef ci::signals::Signal<void( std::vector<syphonServerDescription> )> Syphon
 
 class syphonServerDirectory {
 public:
-	syphonServerDirectory();
-	~syphonServerDirectory();
-	
-    void setup();
-    bool isSetup();
-    int size();
-    
-    bool isValidIndex(int _idx);
-    bool serverExists(std::string _serverName, std::string _appName);
-    bool serverExists(syphonServerDescription _server);
-    syphonServerDescription& getDescription(int _idx);
-    
-    std::vector<syphonServerDescription>& getServerList();
-    void printList();
-	SyphonServerAnnouncedSignal* getServerAnnouncedSignal();
-    SyphonServerUpdatedSignal* getServerUpdatedSignal();
-    SyphonServerRetiredSignal* getServerRetiredSignal();
-    
-    //needs to be public because of the nature of CFNotifications.  please do not call this.
-    void handleNotification(CFStringRef name, CFDictionaryRef userInfo);
-	
+  syphonServerDirectory();
+  ~syphonServerDirectory();
+  
+  void setup();
+  bool isSetup();
+  int size();
+  
+  bool isValidIndex(int _idx);
+  bool serverExists(std::string _serverName, std::string _appName);
+  bool serverExists(syphonServerDescription _server);
+  syphonServerDescription& getDescription(int _idx);
+  
+  std::vector<syphonServerDescription>& getServerList();
+  void printList();
+  SyphonServerAnnouncedSignal* getServerAnnouncedSignal();
+  SyphonServerUpdatedSignal* getServerUpdatedSignal();
+  SyphonServerRetiredSignal* getServerRetiredSignal();
+  
+  //needs to be public because of the nature of CFNotifications.  please do not call this.
+  void handleNotification(CFStringRef name, CFDictionaryRef userInfo);
+  
 private:
-	//void update(ofEventArgs& args);
-    void refresh(bool isAnnounce);
-    void serverAnnounced();
-    void serverUpdated();
-    void serverRetired();
-    
-    void addObservers();
-    void removeObservers();
-    
-    bool CFStringRefToString(CFStringRef src, std::string &dest);
-    
-	bool bSetup;
-    std::vector<syphonServerDescription> serverList;
-    
-    SyphonServerAnnouncedSignal mAnnouncedSignal;
-    SyphonServerUpdatedSignal mUpdatedSignal;
-    SyphonServerRetiredSignal mRetiredSignal;
+  //void update(ofEventArgs& args);
+  void refresh(bool isAnnounce);
+  void serverAnnounced();
+  void serverUpdated();
+  void serverRetired();
+  
+  void addObservers();
+  void removeObservers();
+  
+  bool CFStringRefToString(CFStringRef src, std::string &dest);
+  
+  bool bSetup;
+  std::vector<syphonServerDescription> serverList;
+  
+  SyphonServerAnnouncedSignal mAnnouncedSignal;
+  SyphonServerUpdatedSignal mUpdatedSignal;
+  SyphonServerRetiredSignal mRetiredSignal;
 };
